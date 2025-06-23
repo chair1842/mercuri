f9d82c8127809b3e382b92e4bdd64ae09e0b7762
krispysauce098 <168501170+krispysauce098@users.noreply.github.com>
2025-06-23 09:04:13 +0800

    add delay to init

--- kernel/main.c
+++ kernel/main.c
@@ -12,14 +12,17 @@ void kernel_init() {
     clear_screen();
     memory_init();
     print_string("Memory Initialized\n");
-    delay(100000000); // Delay to simulate some initialization time
+    delay(100000000);
     paging_init();
     print_string("Paging Initialized\n");
+    delay(100000000);
     idt_init();
     print_string("IDT Initialized\n");
+    delay(100000000);
 
 
     print_string("Kernel Initialized\n");
+    delay(100000000);
     clear_screen();
 }
 
