bb7b8e66d75909943eb96f097947b616875f72ee
chair1842 <207965457+chair1842@users.noreply.github.com>
2025-06-28 13:59:24 +0800

    update todo

--- kernel/main.c
+++ kernel/main.c
@@ -10,24 +10,24 @@ void kernel_init() {
     clear_screen();
     memory_init();
     print_string("Memory Initialized\n");
-    delay(100000000);
+    delay(10000000);
     paging_init();
     print_string("Paging Initialized\n");
-    delay(100000000);
+    delay(10000000);
 
     pic_remap(0x20, 0x28);
     print_string("PIC Remapped\n");
-    delay(100000000);
+    delay(10000000);
     idt_init();
     print_string("IDT Initialized\n");
-    delay(100000000);
+    delay(10000000);
 
     keyboard_init();
     print_string("Keyboard Initialized\n");
-    delay(100000000);
+    delay(10000000);
 
     print_string("Kernel Initialized\n");
-    delay(100000000);
+    delay(10000000);
     clear_screen();
 }
 
